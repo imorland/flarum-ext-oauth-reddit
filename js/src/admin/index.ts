44a36a5027585a3ceb7746c6557e14ee82e7b9cc
IanM <ian@morland.me>
2026-04-20 16:26:47 +0100

    chore(2.0): use new import from extension format
    Importing modules from an extension can now be done though `ext:vendor/extension/module-path` format.

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,6 +1,6 @@
 import app from 'flarum/admin/app';
 // @ts-ignore
-import { ConfigureWithOAuthPage } from '@fof-oauth';
+import { ConfigureWithOAuthPage } from '@fof-oauth'; // @TODO: import from `ext:vendor/extension/module-path` format.
 
 app.initializers.add('ianm/oauth-reddit', () => {
   app.extensionData.for('ianm-oauth-reddit').registerPage(ConfigureWithOAuthPage);
